7e8006ab55840049c0ed216ba01a599b40343643
Francisco Florentino de Sousa Filho <ffsf.filho@gmail.com>
2021-06-01 11:14:44 -0300

    Atualização do arquivo README.md

--- Program.cs
+++ Program.cs
@@ -37,16 +37,13 @@ namespace dio_series
                 opcaoUsuario = ObterOpcaoUsuario();
             }
         }
-
     private static void VisualizarSerie()
     {
         Console.WriteLine($"Digite o id da série: ");
         int indiceSerie = int.Parse(Console.ReadLine());
-        
         var serie = repositorio.RetornaPorId(indiceSerie);
         Console.WriteLine(serie);
     }
-
     private static void ExcluirSerie()
     {
         Console.WriteLine("Digite o id da Série: ");
@@ -59,65 +56,18 @@ namespace dio_series
             Console.WriteLine($"A Série foi excluída com sucesso!!!");
         }
     }
-
     private static void AtualizarSerie()
     {
         Console.WriteLine("Digite o id da série: ");
         int indiceSerie = int.Parse(Console.ReadLine());
-        
-        foreach (int i in Enum.GetValues(typeof(Genero)))
-        {
-            Console.WriteLine($"{i}-{Enum.GetName(typeof(Genero),i)}");
-        }
-        
-        Console.WriteLine("Digite o genêro entre as opções acima: ");
-        int entradaGenero = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Digite o Título da Serie: ");
-        string entradaTitulo = Console.ReadLine();
-        
-        Console.WriteLine("Digite o Ano de Início da Série: ");
-        int entradaAno = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Digite a Descrição da Série: ");
-        string entradaDescricao = Console.ReadLine();
-        
-        Serie atualizaSerie = new Serie(id: indiceSerie,
-                                            genero: (Genero)entradaGenero,
-                                            titulo: entradaTitulo,
-                                            ano: entradaAno,
-                                            descricao: entradaDescricao);
-        
+        Serie atualizaSerie = DadosDaSerie(indiceSerie);
         repositorio.Atualiza(indiceSerie, atualizaSerie);
     }
-
     private static void InserirSerie()
     {
         Console.WriteLine($"Inserir nova Série");
-
-        foreach (int i in Enum.GetValues(typeof(Genero)))
-        {
-            Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
-        }
-
-        Console.WriteLine($"Digite o gênero ente as opções acima: ");
-        int entradaGenero = int.Parse(Console.ReadLine());
-
-        Console.WriteLine($"Digite o Título da Série: ");
-        string entradaTitulo = Console.ReadLine();
-
-        Console.WriteLine($"Digite o Ano de Início da Série: ");
-        int entradaAno = int.Parse(Console.ReadLine());
-
-        Console.WriteLine($"Digite a Descrição da Série: ");
-        string entradaDescricao = Console.ReadLine();        
-        
-        Serie novaSerie = new Serie(id: repositorio.ProximoId(),
-                                    genero: (Genero)entradaGenero,
-                                    titulo: entradaTitulo,
-                                    ano: entradaAno,
-                                    descricao: entradaDescricao);
-        
+        int idDaSerie = repositorio.ProximoId();
+        Serie novaSerie = DadosDaSerie(idDaSerie);
         repositorio.Insere(novaSerie);
     }
     private static void ListarSeries()
@@ -138,7 +88,32 @@ namespace dio_series
             Console.WriteLine($"#ID {serie.retornaId()}: - {serie.retornaTitulo()} {(excluido?"*Excluido*": "")}");
         }
     }
+    private static Serie DadosDaSerie(int indiceSerie)
+    {
+        foreach (int i in Enum.GetValues(typeof(Genero)))
+        {
+            Console.WriteLine($"{i}-{Enum.GetName(typeof(Genero), i)}");
+        }
 
+        Console.WriteLine("Digite o genêro entre as opções acima: ");
+        int entradaGenero = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o Título da Serie: ");
+        string entradaTitulo = Console.ReadLine();
+
+        Console.WriteLine("Digite o Ano de Início da Série: ");
+        int entradaAno = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a Descrição da Série: ");
+        string entradaDescricao = Console.ReadLine();
+
+        Serie serie = new Serie(id: indiceSerie,
+                                            genero: (Genero)entradaGenero,
+                                            titulo: entradaTitulo,
+                                            ano: entradaAno,
+                                            descricao: entradaDescricao);
+        return serie;
+    }
     private static string ObterOpcaoUsuario()
         {
             Console.WriteLine();
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Bootcamp MRV .NET Developer
+
 ## Projeto de CRUD básico para cadastro de Séries
+
+Este projeto foi desenvolvido durante o Bootcamp, teve como desafio desenvolver uma aplicação
+básica que fosse capaz de armazenar na mémoria algumas informações sobre séries, onde pude aplicar os conceitos de POO aprendidos até aqui.
+
+### Requisitos
+
+* Windows 7 ou superior
+* .NET versão 5.0.103
+
+### Instruções
+
+1. Fazer Downloading do projeto
+  <a href="https://github.com/ffsf-filho/squad12/archive/main.zip">Download ZIP</a>
+    
+    OU
+    
+    ```git clone  https://github.com/ffsf-filho/dio-series.git```
+2. Descompactar o projeto
+3. Acessar o prompt de comando, para ir até a paste do projeto
+4. Executar o comando de build do dotnet
+   ```
+   dotnet build
+   ```
+5. Executar o programa
+   ```
+   dotnet run
+   ```
+6. Começe Inserindo uma nova série, veja as opções no menu da aplicação.
+<img src=".github/md/teladaaplicao.png" alt="Tela da aplicação">
